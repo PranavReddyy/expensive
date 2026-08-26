e7901a9239de27e73b3320e0fc55d6ce49992d3b
Pranav Reddy <77679001+PranavReddyy@users.noreply.github.com>
2026-08-26 21:02:31 +0530

    ios2

--- app/layout.js
+++ app/layout.js
@@ -6,15 +6,16 @@ export const metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
     ],
     shortcut: "/favicon.ico",
     apple: [
-      { url: "/app-icon-120.png", sizes: "120x120", type: "image/png" },
-      { url: "/app-icon-152.png", sizes: "152x152", type: "image/png" },
-      { url: "/app-icon-167.png", sizes: "167x167", type: "image/png" },
-      { url: "/app-icon-180.png", sizes: "180x180", type: "image/png" },
+      { url: "/apple-touch-icon-120x120.png", sizes: "120x120", type: "image/png" },
+      { url: "/apple-touch-icon-152x152.png", sizes: "152x152", type: "image/png" },
+      { url: "/apple-touch-icon-167x167.png", sizes: "167x167", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
   },
   appleWebApp: {
